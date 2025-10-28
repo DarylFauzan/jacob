a3e03b731672fa2cb769b588f6701a63038c5d1d
Daryl <daryl.fauzan@doku.com>
2025-10-28 18:27:33 +0700

    third push

--- src/main.py
+++ src/main.py
@@ -2,18 +2,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-import os
+import os, tempfile, requests
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes, CommandHandler
 
 from client.orchestrator import orchestrator
+from servers.tools import image_to_data_uri
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 print(TELEGRAM_TOKEN)
 
+# define the bot message when the user start the bot
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("""Assalamualaikum Akhi""")
 
+# text chat handler
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # retrieve the user information
     user = update.message.from_user
@@ -25,10 +28,64 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(response)
 
+# image handler
+async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    user_id = user.id
+
+    photo = update.message.photo[-1]  # largest resolution
+    file = await photo.get_file()
+    file_bytes = requests.get(file.file_path).content
+
+    image_data_uri = image_to_data_uri(file_bytes)
+    user_question = update.message.caption or ""
+
+    message = [
+        {"type": "text", "text": user_question},
+        {"type": "image_url", "image_url": image_data_uri},
+    ]
+
+    response = await orchestrator(user_id, message, model_name="gpt-4o-mini")
+    await update.message.reply_text(response.content)    
+
+# voice not chat handler
+async def voice_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # retrieve the user information
+    user = update.message.from_user
+    user_id = user.id
+
+    voice_note = update.message.voice
+    file_id = voice_note.file_id
+
+    file_object = await context.bot.get_file(file_id)
+
+    # Create a temporary file path. Telegram voice notes are OGG/Opus.
+    with tempfile.NamedTemporaryFile(suffix=".ogg", delete=False) as temp_file:
+        temp_path = temp_file.name
+        
+        # Download the file
+        await file_object.download_to_drive(custom_path=temp_path)
+    
+    # send the message to speech to text model
+    with open(temp_path, 'rb') as f:
+        files = {'file': ('voice.ogg', f, 'audio/ogg')}
+        response = requests.post(
+                EXTERNAL_API_URL, 
+                files=files, 
+                # headers=headers,
+                timeout=30 # Set a timeout for the request
+            )
+        string_message = response.text
+
+    response = await orchestrator(user_id, string_message, model_name="gpt-4o-mini")
+
+    await update.message.reply_text(response)
+
 if __name__ == "__main__":
     print("initializing bot...")
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT, chat))
+    app.add_handler(MessageHandler(filters.VOICE, voice_chat))
     app.run_polling()
     print("chatbot successfully initialized")
--- src/servers/tools.py
+++ src/servers/tools.py
@@ -1,3 +1,29 @@
+from langchain_openai import OpenAIEmbeddings
+from sqlalchemy import create_engine, text
+import numpy as np
+import pandas as pd
+import base64, re, requests
+from nltk.corpus import stopwords
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+stop_words = set(stopwords.words("indonesian"))
+
+embedding_model = OpenAIEmbeddings(model = "text-embedding-3-large")
+
+# Connect to the database
+db_url = "postgresql+psycopg2://daryl:201178@localhost:5432/biometric_core"
+engine = create_engine(db_url)
+
+def image_to_data_uri(file_bytes, mime_type="image/jpeg"):
+    return f"data:{mime_type};base64,{base64.b64encode(file_bytes).decode()}"
+
+def extract_keywords(text):
+    words = re.findall(r'\w+', text.lower())
+    return [w for w in words if w not in stop_words]
+
 system_message = """
 Anda adalah seorang customer care di DOKU, salah satu company payment gateaway terbesar di Indonesia.
 
@@ -21,13 +47,107 @@ Tugas anda:
 3. Menjelaskan kepada user dokumen apa saja yang dibutuhkan jika ingin mendaftar sebagai merchant DOKU.
 
 Anda akan diberikan tools untuk membantu anda dalam menjalani tugas anda:
-1. Untuk mendapatkan informasi mengenai produk DOKU, gunakan tools "product_list"
+1. Untuk mendapatkan informasi mengenai produk DOKU, gunakan tools "product_recommendation"
 2. Untuk mendapatkan informasi mengenai alur pendaftaran, gunakan tools "registration_procedure"
-3. 
+3. Jika user ingin menggunakan informasi mengenai dokumen apa saja yang harus disiapkan, gunakan tools "document_list"
 
 Anda tidak diperbolehkan menjawab pertanyaan yang di luar konteks. 
 Anda hanya menggunakan tools ketika Anda butuh dan tidak berulang.
 Anda juga hanya diperbolehkan menjawab berdasarkan informasi yang anda punya.
 Jika anda tidak mempunyai akses ke informasi tersebut, respon dengan sopan bahwa anda tidak tau.
+Jika anda tidak memahami penjelasan dari user, jangan sungkan untuk bertanya ke user.
 Jika user menanyakan informasi yang di luar konteks, tolak pertanyaan itu dengan sopan.
 """
+
+def ktp_parser(image: str) -> dict:
+    """use this function if you want to parse Indonesia ID Card. 
+    
+    Input parameter:
+    1. image: base64 string image."""
+
+    response = response = requests.request(
+        "POST", "http://172.17.20.68:5000/api/v2/ocr/base64",
+        data={"image": image},
+    )
+
+    return response.json()
+
+def product_recommendation(text):
+    """use this function to fetch product list that suits the user needs. this function will return the list of recommendation product based on the user description
+    input:
+    1. text: str = a string that describe the user company.
+    
+    output:
+    output format will be in dictionary where the key is 
+    1. product_id
+    2. product_description
+    3. relevant_score: measure the relevancy of the product with the user description. the score is [0, 1] where 1 is the most relevant product"""
+
+    # set up semantic search
+    embd = embedding_model.embed_query(text)
+    norm_embd = np.array(embd) / np.linalg.norm(embd) # unit vector
+    print(norm_embd.shape)
+    norm_embd = norm_embd.flatten().tolist()
+    # set up keyword search
+    
+    list_keywords = extract_keywords(text)
+    keywords_logic = ""
+
+    for w in list_keywords:
+        if len(keywords_logic) != 0:
+            keywords_logic+= "|"
+        keywords_logic += f"{w}"
+    
+
+    query = text(f"""
+WITH semantic_search AS (
+    SELECT  
+        id,
+        embeddings <=> ARRAY{norm_embd}::vector AS semantic_score
+    FROM hackathon
+),
+keyword_search AS (
+    SELECT 
+        id,
+        category,
+        chunks,
+        ts_rank_cd(
+            to_tsvector('indonesian', chunks),
+            to_tsquery('{keywords_logic}'),
+            32
+        ) AS keyword_score
+    FROM hackathon
+)
+
+SELECT 
+    s.id as product_id,
+    k.chunks as product_description,
+    0.5 * (1 - s.semantic_score/2) + 0.5 * k.keyword_score AS relevant_score
+FROM semantic_search AS s
+JOIN keyword_search AS k
+    ON s.id = k.id
+WHERE k.category = 'product'
+ORDER BY final_score DESC
+LIMIT 2;
+    """)
+
+    dfx = pd.read_sql_query(query,con=engine)
+    table_info = dfx.to_dict(orient = "records")
+    return table_info
+
+
+def registration_procedure(text):
+    """use this function if you want to explain the registration process to the user"""
+
+    query = text("""
+SELECT 
+    id as product_id,
+    chunks as product_description
+FROM semantic_search
+WHERE k.category = 'registration_procedure'
+LIMIT 1;
+""")
+    
+    dfx = pd.read_sql_query(query,con=engine)
+    table_info = dfx.to_dict(orient = "records")
+    return table_info
